7da87271e45b602e8280967d511b6f168a50ad31
Kamil Hojka <kamhojka@gmail.com>
2024-01-15 22:02:44 +0100

    Add to mode toggle button title prop

--- components/common/mode-toggle-button.tsx
+++ components/common/mode-toggle-button.tsx
@@ -14,6 +14,11 @@ export function ModeToggleButton() {
     <Button
       variant="outline"
       size="icon"
+      title={
+        currentTheme === "light"
+          ? "Switch to dark mode"
+          : "Switch to light mode"
+      }
       onClick={() => setTheme(currentTheme === "light" ? "dark" : "light")}
     >
       <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
