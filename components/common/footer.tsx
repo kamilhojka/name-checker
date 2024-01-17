08460ef603e6690a45937a0803e3b5506cc212a5
Kamil Hojka <kamhojka@gmail.com>
2024-01-17 13:35:18 +0100

    Update style of 'Footer' component

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -4,12 +4,12 @@ import { GITHUB_PROFILE_URL } from "@/lib/constants";
 
 export function Footer() {
   return (
-    <footer className="container w-full flex h-14 items-center bg-secondary/45">
-      <p className="text-sm text-muted-foreground">
+    <footer className="w-full flex h-14 items-center bg-secondary/45">
+      <p className="container text-sm text-muted-foreground">
         website created by{" "}
         <Link
           href={GITHUB_PROFILE_URL}
-          className="font-bold underline decoration-2 cursor-pointer after:content-['_↗'] hover:text-primary"
+          className="font-bold underline decoration-2 cursor-pointer hover:text-primary"
           target="_blank"
         >
           kamilhojka
